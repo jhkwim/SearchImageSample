c0331fd28e22cd96dc63a6596a7f2cf9902e8f52
jhkim <jhkim@mobileleader.com>
2023-09-08 10:04:40 +0900

    불필요 dependency 제거

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -49,10 +49,6 @@ secrets {
 }
 
 dependencies {
-
-    implementation(libs.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
